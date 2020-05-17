4fdca54ccaf283653d37699ad37444c0545fc608
Moon <moonlikesinging@qq.com>
2020-05-17 22:50:48 +0800

    Update to add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="container-fluid">
+        <div class="row" v-lazy:background-image = footerBgImg style="background-repeat: no-repeat; background-size: 100% 100%; height: auto">
+            <div class="offset-1"></div>
+            <div class="col-lg-7 col-7">
+                <img v-lazy = footerDesc  class="img-fluid"/>
+            </div>
+            <div class="col-lg-3 col-3">
+                <img v-lazy = qrCode  class="img-fluid" style="margin-top: 1%">
+            </div>
+
+        </div>
+    </div>       
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  components:{},
+  props:{},
+  data(){
+    return {
+        footerBgImg : require('../assets/img/footer-bg.png'),
+        footerDesc : require('../assets/img/footer.png'),
+        qrCode : require('../assets/img/QRcode.png'),
+    }
+  },
+  watch:{},
+  computed:{},
+  methods:{},
+  created(){},
+  mounted(){}
+}
+</script>
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <Banner></Banner>
     <PoetryExampleInHome></PoetryExampleInHome>
+    <Footer></Footer>
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 // @ is an alias to /src
 import Banner from '@/components/Banner.vue'
 import PoetryExampleInHome from '@/components/PoetryExampleInHome.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'Home',
   components: {
     Banner,
     PoetryExampleInHome,
+    Footer,
   }
 }
 </script>
